1ef5a66ecb2eb0278e140a4e3da9f95e6d8f266b
Andrei Assa <andrei.assa@gmail.com>
2021-07-17 14:39:14 -0400

    Progress on DataValidator

--- validator/validator.py
+++ validator/validator.py
@@ -2,6 +2,9 @@
 Defines DataValidator class.
 """
 
+from pandas import DataFrame
+from typing import Optional
+
 """
 
     Requirements:
@@ -31,6 +34,7 @@ Defines DataValidator class.
 
 """
 
+
 # Defines custom-defined data validation Exceptions
 class NonUniqueValueError(Exception):
     """Exception raised when data values in column should be, but are not unique."""
@@ -39,12 +43,14 @@ class NonUniqueValueError(Exception):
         self.message = message
         super().__init__(self.message)
 
+
 class NonNullError(Exception):
     """Excaption raised when data value is missing, but it should be not null."""
     def __init__(self, column_name, message="Some value in the column is null. Please fill in missing values."):
         self.column_name = column_name
         self.message = message
         super().__init__(self.message)
+
 
 class DataTypeException(Exception):
     """Exception raised when data type is incorrect."""
@@ -55,15 +61,30 @@ class DataTypeException(Exception):
         super().__init__(self.message)
 
 
+class FieldNotExistsError(Exception):
+    def __init__(self, field_name):
+        message = "The field name '%s' does not exist in the data" % field_name
+        super().__init__(message)
+
+
 class DataValidator(object):
 
-    def __init__(self, order_id_col_name=None, product_id_col_name=None):
+    def __init__(
+            self,
+            *,
+            data: DataFrame,
+            order_id_col_name: Optional[str] = None,
+            product_id_col_name: Optional[str] = None
+            ):
+        self.data = data
         self._order_id_col_name = "order_id" if order_id_col_name is None else order_id_col_name
         self._product_id_col_name = "product_id" if product_id_col_name is None else product_id_col_name
-        self._data_format = None
 
+        for field in self._order_id_col_name, self._product_id_col_name:
+            if field not in self.data.columns:
+                raise FieldNotExistsError(field_name=field)
 
-    def validate(self, transformed_data):
+    def validate(self):
         # identify whether data is one-row-per-product or one-row-per-order format
         # Don't assume product_ids and order_ids are integers
         
@@ -71,81 +92,56 @@ class DataValidator(object):
         if one-row-per-order:
             
         """
-        
-        self._order_ids_unique = False
-        self._product_ids_are_list = False
-        self._product_ids_are_str_or_int = False
-        self._product_ids_in_list_are_int_strings = False
-        
-        # if self._order_ids_unique and self._product_ids_are_list:
-            # self._short_format = True
-        # elif not self._order_ids_unique (and self._order_ids_str_or_int) and self._product_ids_are_str_or_int:
-            # self._long_format = True
 
-        
         self._long_format = False
         self._short_format = False
-        
-        if not isinstance(transformed_data["order_id"][0], list): # if True, is one-row-per-product format (long-format)
+
+        def _check_list_like(value):
+            if isinstance(value, list):
+                return True
+            elif isinstance(value, str) and "[" in value and "]" in value:
+                return True
+            else:
+                return False
+
+        order_ids_unique = self.data[self._order_id_col_name].is_unique
+        product_ids_as_lists = list(self.data[self._product_id_col_name].apply(_check_list_like))
+        all_product_ids_are_lists = all(product_ids_as_lists)
+
+        if order_ids_unique and all_product_ids_are_lists:
+            self._short_format = True
+            self._long_format = False
+        elif not order_ids_unique:
+            self._short_format = False
             self._long_format = True
 
-        elif not isinstance(transformed_data["product_id"][0], list): # if True, is one-row-per-order format (short-format)
-            self._short_format = True
-        else:
-            raise RuntimeError
-
-
-        # TODO: serialize all columns astype(str), put the following two lines there:
-        transformed_data["order_id"] = transformed_data["order_id"].astype(str)
-        transformed_data["product_id"] = transformed_data["product_id"].astype(str)
-
-
-        if self._short_format: # If data is one-row-per-order (short format):
-            
-            # Check that each order id is UNIQUE. O(n)
-            if len(transformed_data) != len(set(transformed_data["order_id"])): 
-                raise NonUniqueValueError("order_id")
-
-            # Check that each order id is correct data type (INTEGER).
-            if not all([isinstance(x, int) for x in transformed_data["order_id"]]):
-                raise DataTypeException(column_name="order_id", data=x)
-            # Check that each product id is correct data type (INTEGER).
-            if not all([isinstance(x, int) for y in transformed_data["product_id"]] for x in y):
-                raise DataTypeException(column_name="product_id", data=x)
-
-            # Check that all orders are matched with at least one product. Otherwise, there exists a NULL field.
-            for x in transformed_data["product_id"]:
-                if not x:
-                    raise NonNullError("product_id")
-
-
-        if not self._long_format: # If data is one-row-per-product (long format):
-            
-            # Check that each product id is UNIQUE. O(n)
-            if len(list(transformed_data["product_id"])) != len(set(transformed_data["product_id"])): 
-                raise NonUniqueValueError("product_id")
-
-            # Check that each product id is correct data type (INTEGER).
-            if not all([isinstance(x, int) for x in transformed_data["product_id"]]):
-                raise DataTypeException(column_name="product_id", data=x)
-            # Check that each order id is correct data type (INTEGER).
-            if not all([isinstance(x, int) for y in transformed_data["order_id"]] for x in y):
-                raise DataTypeException(column_name="order_id", data=x)
-            
-            """
-                order_id    product_id
-                1           []
-            """
-
-            # Check that all products are matched with at least one order. Otherwise, there exists a NULL field.
-            for x in transformed_data["order_id"]:
-                if not x:
-                    raise NonNullError("order_id")
-
-        # TODO: Check other columns are also of correct data type.
-        # TODO: Check MIN and MAX values?
+        return self._long_format or self._short_format
 
 
 if __name__ == '__main__':
-    data_validator = DataValidator()
-    data_validator.validate(transformed_data) # validates data that was transformed from `transformer.py`
+    import os
+    import pandas as pd
+    from transformer import DataTransformer
+
+    # Set data directory
+    data_dir = "data" if "data" in os.listdir() else "../data"
+    filename = "order_products_prior_subset.csv"
+    filepath = f"{data_dir}/{filename}"
+
+    data_transformer = DataTransformer(filepath)
+    transformed_data = data_transformer.transform()
+    non_transformed_data = data_transformer.data.copy()
+
+    invalid_data = pd.DataFrame()
+    invalid_data["order_id"] = [1, 2, 3]
+    # Invalid because data contains tuples instead of lists
+    invalid_data["product_id"] = [("one", "two"), ("three", "four"), ("five", "six")]
+    
+    for name, value in [
+        ("long", non_transformed_data),
+        ("short", transformed_data),
+        ("invalid", invalid_data)
+            ]:
+        data_validator = DataValidator(data=value)
+        result = data_validator.validate()
+        print(f"format={name}", f"valid-result={result}")
