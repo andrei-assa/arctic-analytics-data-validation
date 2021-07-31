be177ec451a87493683aac87010016fff7783fef
Andrei Assa <andrei.assa@gmail.com>
2021-07-30 20:10:11 -0400

    Updated market_basket_analysis.py

--- src/algorithms/market_basket_analysis.py
+++ src/algorithms/market_basket_analysis.py
@@ -6,8 +6,6 @@ from mlxtend.frequent_patterns import apriori, association_rules
 from src.utils.utils import get_project_root
 
 project_root = get_project_root()
-sys.path.insert(0, project_root)
-from validation.transformer import DataTransformer
 
 data_directory = f"{project_root}/data"
 orders_fp = "order_products_prior_subset.csv"
@@ -17,19 +15,13 @@ orders = pd.read_csv(f"{data_directory}/{orders_fp}")
 products = pd.read_csv(f"{data_directory}/{products_fp}")
 
 orders = orders.merge(products, on="product_id", how="left")
-baking_supplies = orders[orders.department_id==10]
+baking_supplies = orders[orders.department_id == 7]
 baking_supplies_order_ids = list(baking_supplies.order_id.unique())
 subset = orders[orders.order_id.isin(baking_supplies_order_ids)]
 subset = subset[["order_id", "product_name"]]
+subset["ordered"] = 1
 
-transformer = DataTransformer(data=subset)
-
-raise KeyboardInterrupt
-
-subset = subset.set_index(["order_id", "product_name"])
-subset["orders"] = 1
-subset = subset.unstack().fillna(0)
-col_names = [x[1] for x in subset.columns]
-subset.columns = col_names
-
-# baking_supplies.unstack()
+basket = subset.groupby(['order_id', 'product_name'])["ordered"]
+basket_matrix = basket.sum().unstack().reset_index().fillna(0).set_index('order_id')
+itemsets = apriori(basket_matrix, min_support=0.005, use_colnames=True)
+rules = association_rules(itemsets)
