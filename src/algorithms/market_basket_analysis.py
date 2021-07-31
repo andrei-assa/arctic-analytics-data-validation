05a5df0458e3bf7fa42f2f71ea6ec2ac0a10e042
Andrei Assa <andrei.assa@gmail.com>
2021-07-31 00:18:07 +0000

    Updated market_basket_analysis again

--- src/algorithms/market_basket_analysis.py
+++ src/algorithms/market_basket_analysis.py
@@ -15,13 +15,11 @@ orders = pd.read_csv(f"{data_directory}/{orders_fp}")
 products = pd.read_csv(f"{data_directory}/{products_fp}")
 
 orders = orders.merge(products, on="product_id", how="left")
-baking_supplies = orders[orders.department_id == 7]
-baking_supplies_order_ids = list(baking_supplies.order_id.unique())
-subset = orders[orders.order_id.isin(baking_supplies_order_ids)]
+subset = orders[orders.department_id ==5]
 subset = subset[["order_id", "product_name"]]
 subset["ordered"] = 1
 
 basket = subset.groupby(['order_id', 'product_name'])["ordered"]
 basket_matrix = basket.sum().unstack().reset_index().fillna(0).set_index('order_id')
-itemsets = apriori(basket_matrix, min_support=0.005, use_colnames=True)
+itemsets = apriori(basket_matrix, min_support=0.001, use_colnames=True)
 rules = association_rules(itemsets)
