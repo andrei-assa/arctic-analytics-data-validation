6f81aef18bcb74402b3089f2917392c53315daef
HDmoviemaker <38936045+hardeepkumar9191@users.noreply.github.com>
2021-08-11 17:50:07 -0400

    Updates

--- src/streamlit_app/app.py
+++ src/streamlit_app/app.py
@@ -11,6 +11,9 @@ root_directory = str(this_file.parent.parent.absolute())
 sys.path.insert(0, root_directory)
 from validation.validator import DataValidator
 
+
+columndict = {} #declaring a global variable for it to collect the columns names and type
+
 # Security
 #passlib,hashlib,bcrypt,scrypt
 import hashlib
@@ -80,14 +83,15 @@ def main():
             logged_in_successfully = login_user(username,check_hashes(password,hashed_pswd))
             if logged_in_successfully:
                 st.success("Logged In as {}".format(username))
-                options_for_task_dropdown = ["Add Post", "Market Basket Analysis", "Profiles"]
+                options_for_task_dropdown = ["Market Basket Analysis", "Profiles"]
                 user_selected_task = st.selectbox("Task", options_for_task_dropdown)
                 if user_selected_task == "Add Post":
                     st.subheader("Add Your Post")
                 #     Placeholder for some additional stuff
 
                 elif user_selected_task == "Market Basket Analysis":
-                    market_basket_analysis()
+                    
+                    ()
 
                 elif user_selected_task == "Profiles":
                     st.subheader("User Profiles")
@@ -108,6 +112,28 @@ def main():
             st.success("You have successfully created a valid Account")
             st.info("Go to Login Menu to login")
 
+def filetypeselecter():
+    file_type = mimetypes.guess_type(uploaded_file.name)
+
+    if file_type[0] in ['text/plain', 'text/tab-separated-values', 'text/csv']: #csv types
+        uploaded_data_df = pd.read_csv(uploaded_file, nrows=100)
+
+    elif file_type[0] in ['application/json']: # json types
+        uploaded_data_df = pd.read_json(uploaded_file, nrows=100)
+
+    elif file_type[0][-5:] in ['sheet', 'excel']: # excel types
+        uploaded_data_df = pd.read_excel(uploaded_file, nrows=100)
+
+    else:
+        print("This file does not a permitted extension. Please upload either .csv, .tsv, .json, .xls, .xlsx, or .txt")
+
+@st.cache
+def load_data(datafile,nrows):
+    data = pd.read_csv(datafile, nrows=nrows)
+    #lowercase = lambda x: str(x).lower()
+    #data.rename(lowercase, axis='columns', inplace=True)
+    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
 def market_basket_analysis():
     st.subheader("Market Basket Analysis")
@@ -115,20 +141,14 @@ def market_basket_analysis():
     # TODO: Add error handling for empty file
     if uploaded_file is not None:
 
-        # File type handling
-        file_name = uploaded_file.name
-        file_extension = os.path.splitext(file_name)[1]
-        print("\nUploaded File Name: ", file_name, "\tFile Extension: ", file_extension)
-
-        loader_function_dictionary = {'.txt': pd.read_csv, '.tsv': pd.read_csv, '.csv': pd.read_csv, \
-            '.json': pd.read_json, '.xls': pd.read_excel, '.xlsx': pd.read_excel}
-        loader_function = loader_function_dictionary[file_extension] if file_extension in loader_function_dictionary else \
-            "This file does not a permitted extension. Please upload either .csv, .tsv, .json, .xls, .xlsx, or .txt"
-
-        print("\nData loading to frame...")
-
-        uploaded_data_df = loader_function(uploaded_file)
-        print("\nData displayed to frame.")
+        # TODO: Implement file type handling
+        # File type handling (Note: may change to Strategy pattern)
+        st.write(uploaded_file.name)
+        data_load_state = st.text('Loading data...')
+        uploaded_data_df = load_data(uploaded_file,100)
+        data_load_state.text("Done! (using st.cache)")
+        # if st.checkbox('Show raw data'):
+        #uploaded_data_df = pd.read_csv(uploaded_file, nrows=100)
 
         validator = DataValidator(
             data=uploaded_data_df
@@ -137,30 +157,19 @@ def market_basket_analysis():
             # TODO: Validator should return a more specific reason why data was not valid.
             st.write("File is valid")
             st.write(uploaded_data_df)
+            st.subheader('Raw data')
+            for x in uploaded_data_df.columns:
+                st.subheader(x)
+                options = st.multiselect('Specify the column type for ' + x, ['String', 'Int', 'Float'], ['Int', 'String'])
+                #st.write('You selected:', options)
+                columndict[x] = options
+            st.write('You selected:', columndict)
+
         else:
             # TODO: Accept user input to fix the problem specified above
             # Allow user to try again?
             st.write("File is not valid")
 
-    DATE_COLUMN = 'date/time'
-    DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-    @st.cache
-    def load_data(nrows):
-        data = pd.read_csv(DATA_URL, nrows=nrows)
-        lowercase = lambda x: str(x).lower()
-        data.rename(lowercase, axis='columns', inplace=True)
-        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-        return data
-
-    data_load_state = st.text('Loading data...')
-    data = load_data(10000)
-    data_load_state.text("Done! (using st.cache)")
-    # if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-    st.subheader('Columns in this dataset')
 
     
     #Issue #9: Connect the file upload function with the dropdown options to allow for selection of columns types
@@ -173,11 +182,6 @@ def market_basket_analysis():
     #for column_name, data_type in remap_dtypes_dictionary.items(): data[column_name] = data[column_name].astype(
     #data_type)
     
-
-    for x in data.columns:
-        st.subheader(x)
-        options = st.multiselect('Specify the column type for ' + x, ['String', 'Int', 'Float'], ['Int', 'String'])
-        st.write('You selected:', options)
 
 
 if __name__ == '__main__':
